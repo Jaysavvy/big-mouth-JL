d6146ca078fb17fb3c9c1b419102049f48a22f06
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-12-10 16:16:53 -0500

    Allowing lambda to server html as call back

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,18 +1,20 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const co = require('co');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'))
+
+
+module.exports.hello = co.wrap(function* (event, context, callback) {
+  let html = yield loadHtml();
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: html,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8',
+    },
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  callback(null, response);
+
+});

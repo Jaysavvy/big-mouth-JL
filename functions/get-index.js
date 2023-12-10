62cdf1a357e95a0596c7ac91b2b847506a98d0ef
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-12-10 16:56:00 -0500

    servered function for html site via lambda

--- functions/get-index.js
+++ functions/get-index.js
@@ -4,17 +4,32 @@ const co = require('co');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'))
 
+var html;
 
-module.exports.hello = co.wrap(function* (event, context, callback) {
+
+function* loadHtml() {
+  if(!html){
+    html = yield fs.readFileAsync('static/index.html', 'utf-8');
+  }
+
+  return html;
+}
+
+
+module.exports.handler = co.wrap(function* (event, context, callback) {
   let html = yield loadHtml();
-  return {
+  const response = {
     statusCode: 200,
     body: html,
     headers: {
       'Content-Type': 'text/html; charset=UTF-8',
     },
+
+    
   };
 
   callback(null, response);
 
-});
+})
+
+
